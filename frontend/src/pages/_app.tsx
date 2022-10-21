80461b8c4a85fcbf312579e6a1418bed9bebda24
Buooy <aaron.lee@buooy.com>
2022-10-21 16:19:59 +0800

    feat(mint): added minting site

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,29 +4,26 @@ import Web3Provider from '@/components/Web3Provider'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
+	getLayout?: (page: ReactElement) => ReactNode
 }
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
+	Component: NextPageWithLayout
 }
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
 	// Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
+	const getLayout = Component.getLayout ?? (page => page)
 
-	return  (
-		<ThemeProvider attribute="class">
-			<Web3Provider>
-				{
-					getLayout(
-						<Component {...pageProps} />
-					)
-				}
-			</Web3Provider>
-		</ThemeProvider>
+	return (
+		<ChakraProvider>
+			<ThemeProvider attribute="class">
+				<Web3Provider>{getLayout(<Component {...pageProps} />)}</Web3Provider>
+			</ThemeProvider>
+		</ChakraProvider>
 	)
 }
 
