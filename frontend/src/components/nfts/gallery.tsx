994190eb1de8818c0286700ae18bd2c1969445d6
Buooy <aaron.lee@buooy.com>
2022-10-21 21:21:56 +0800

    fix(gallery): updated instrinsic attributes

--- frontend/src/components/nfts/gallery.tsx
+++ frontend/src/components/nfts/gallery.tsx
@@ -1,54 +1,48 @@
 /* eslint-disable @next/next/no-img-element */
 import { FC, useEffect } from 'react'
 import Image from 'next/image'
-import { useWalletContext } from '@/providers/wallet';
-import frameSingle from '@/assets/images/frame-single.jpeg';
+import { useWalletContext } from '@/providers/wallet'
+import frameSingle from '@/assets/images/frame-single.jpeg'
 
-const Gallery: FC = ({address, contractAddresses}: { address: string, contractAddresses: string[] }) => {
+const Gallery = ({ address, contractAddresses }: { address: string; contractAddresses: string[] }) => {
 	const { nfts, fetchNfts } = useWalletContext()
 
-  const getNfts = async () => {
-    await fetchNfts(address, contractAddresses);
-  }
+	const getNfts = async () => {
+		await fetchNfts(address, contractAddresses)
+	}
 
 	useEffect(() => {
-    getNfts();
-	}, []);
+		getNfts()
+	}, [])
 
 	return (
-    <div className="flex justify-center">
-      {
-        nfts && nfts.map(nft => (
-          <div
-            key={nft.title}
-            className="flex w-1/3"
-            style={{
-              backgroundImage: `url("${frameSingle.src}")`,
-              backgroundSize: "contain",
-              backgroundColor: "#f9f8f4",
-              backgroundRepeat: "no-repeat",
-              height: "100vh",
-            }}
-          >
-            <div
-              className="relative text-center"
-              style={{
-                width: "40%",
-                marginLeft: "29%",
-                marginTop: "35%"
-              }}
-            >
-              <img
-                className="pb-2 flex"
-                src={nft.media[0].gateway}
-                alt="image"
-              />
-            </div>
-          </div>
-        ))
-      }
-      
-    </div>
+		<div className="flex justify-center">
+			{nfts &&
+				nfts.map(nft => (
+					<div
+						key={nft.title}
+						className="flex w-1/3"
+						style={{
+							backgroundImage: `url("${frameSingle.src}")`,
+							backgroundSize: 'contain',
+							backgroundColor: '#f9f8f4',
+							backgroundRepeat: 'no-repeat',
+							height: '100vh',
+						}}
+					>
+						<div
+							className="relative text-center"
+							style={{
+								width: '40%',
+								marginLeft: '29%',
+								marginTop: '35%',
+							}}
+						>
+							<img className="pb-2 flex" src={nft.media[0].gateway} alt="image" />
+						</div>
+					</div>
+				))}
+		</div>
 	)
 }
 
